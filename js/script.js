8dd8d9a3bd6f0d596c59337c0dc300fe630f6156
albertholskog <91052944+albertholskog@users.noreply.github.com>
2022-02-14 13:04:56 +0100

    done done

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
-// const rawKey = "a0191d2613594ed4b734b41b7c4b6dd2";
-// const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${rawKey}`;
-// console.log("start");
-
-// const conteiner = document.querySelector(".conteiner");
-// async function getApi() {
-//   try {
-//     const response = await fetch(url);
-//     const results = await response.json();
-//     const api = results;
-//     console.log(api);
-//     for (let i = 0; i < api.lenght; i++) {
-//       const Name = url[i].name;
-//       const Rating = url[i].rating;
-//       const Tag = url[i].tags;
-//       conteiner.innerHTML += `<div><li>${Name}${Rating}${Tag}</li></div>`;
-//     }
-//   } catch (error) {
-//     console.log("error, noe er feil");
-//     conteiner.innerHTML += `<p> noe galt</p>`;
-//   } finally {
-//     console.log("all done");
-//   }
-// }
-// getApi();
 const rawKey = "a0191d2613594ed4b734b41b7c4b6dd2";
+const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${rawKey}`;
+const conteiner = document.querySelector(".box");
+
 async function getApi() {
-  const response = await fetch(
-    `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${rawKey}`
-  );
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+    const getArray = results.results;
+    conteiner.innerHTML = "";
+    console.log(getArray);
+    for (let i = 0; i < getArray.length; i++) {
+      if (i === 8) {
+        break;
+      }
+      const name = getArray[i].name;
+      const rating = getArray[i].rating;
+      const numberOfTags = getArray[i].tags;
+      conteiner.innerHTML += `<div class="games"><ul class="api-list">
+                  <li>Game title: ${name}
+                  Game rating: ${rating}
+                  Number of tags: ${numberOfTags.length}
+                  </li></ul></div>`;
+    }
+  } catch (error) {
+    console.log(error, "Could not get API");
+    conteiner.innerHTML = `<p class="error">Promblems with the API get</p>`;
+  } finally {
+    console.log("All done");
+  }
 }
+
+getApi();
